77bc24e33decde06e0c8bfa5cebbc5270523db3e
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2025-12-28 21:27:32 +0700

    progress 1%

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <header className="p-4 mt-2 ">
+            <nav>
+                <h1 className="text-4xl text-emerald-500 font-medium"><span className="font-bold">Panen</span>Kita</h1>
+                <ul>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default App;
