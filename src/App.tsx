e6f7866093ce86abf476f00191a6561484f63af6
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2026-01-08 21:06:48 +0700

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
-import LoginPage from './components/pages'
+import LoginPage from './components/pages/LoginPage';
+import RegisterPage from './components/pages/RegisterPage';
+import CustomerHome from './components/pages/CustomerHome';
+import DashboardProducer from './components/pages/DashboardProducer';
 
 
 function App(){
